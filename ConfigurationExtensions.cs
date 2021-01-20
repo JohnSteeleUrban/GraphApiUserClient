58bf4b7d1231451cfb366a4f7ed42aa4185364d3
John Urban <johnsteeleurban@gmail.com>
2021-01-20 13:08:43 -0700

    Add files via upload

---
+++ ConfigurationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace GraphApi.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetClientId(this IConfiguration configuration)
+        {
+            return configuration["AzureAdB2C:ClientId"] ?? string.Empty;
+        }
+
+        public static string GetTenantId(this IConfiguration configuration)
+        {
+            return configuration["AzureAdB2C:TenantId"] ?? string.Empty;
+        }
+
+        public static string GetClientSecret(this IConfiguration configuration)
+        {
+            return configuration["B2CSettings:ClientSecret"] ?? string.Empty;
+        }
+    }
+}
