7ef29cfb7fe5d16eac5dacdb53702fb63373be72
John Urban <johnsteeleurban@gmail.com>
2021-01-20 12:49:19 -0700

    Add files via upload

---
+++ Extensions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using GraphApi.Models.Shared;
+using GraphApi.Models.Users;
+
+using Microsoft.Graph;
+
+using System.Collections.Generic;
+using System.Linq;
+using AzureB2CApi.Helpers;
+
+namespace GraphApi.Extensions
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Gets then removes the first specified number of items in a List.
+        /// If the specified number to remove is greater than the count of the list, then all items will
+        /// be removed and returned.  The originating list will then be empty.
+        /// </summary>
+        /// <typeparam name="T">The Type of list</typeparam>
+        /// <param name="list">A collection of Type T</param>
+        /// <param name="numToRemove">The number of items to remove from the list.</param>
+        /// <returns></returns>
+        public static IList<T> GetAndRemove<T>(this IList<T> list, int numToRemove)
+        {
+            lock (list)
+            {
+                List<T> values = new List<T>();
+
+                if (list.Count > numToRemove)
+                {
+                    while (numToRemove != 0)
+                    {
+                        numToRemove--;
+                        values.Add(list[0]);
+                        list.RemoveAt(0);
+                    }
+                }
+                else
+                {
+                    values.AddRange(list);
+                    list.Clear();
+                }
+
+                return values;
+            }
+        }
+
+        public static User ToUser(this UserModel userModel)
+        {
+            var user = new User
+            {
+                Id = userModel.Id,
+                DisplayName = userModel.DisplayName,
+                Mail = userModel.Mail,
+                Surname = userModel.Surname,
+                GivenName = userModel.GivenName,
+            };
+            return user;
+        }
+
+        public static User CreateUser(this UserModel model)
+        {
+            return new User
+            {
+                AccountEnabled = true,
+                DisplayName = model.DisplayName,
+                OtherMails = new List<string> { model.Mail },
+                Mail = model.Mail,
+                MailNickname = model.DisplayName,
+                UserPrincipalName = $"{Guid.NewGuid()}@{Program.Configuration.GetSection("AzureAdB2C:Domain").Value}",
+                Identities = new List<ObjectIdentity> {
+                    new ObjectIdentity
+                        {SignInType = "userName", Issuer = "lodsaccb2c.onmicrosoft.com", IssuerAssignedId = model.DisplayName},
+                    new ObjectIdentity
+                        {SignInType = "emailAddress", Issuer = "lodsaccb2c.onmicrosoft.com", IssuerAssignedId = model.Mail}
+                },
+                GivenName = model.GivenName,
+                Surname = model.Surname,
+                PasswordProfile = new PasswordProfile { ForceChangePasswordNextSignIn = true, ForceChangePasswordNextSignInWithMfa = false, Password = PasswordGenerator.GeneratePassword() },
+            };
+        }
+
+        public static PagerModel<UserModel> ToPagerModel(this IList<User> users, string search, int page = 1, int pageSize = 10)
+        {
+            var userModels = users.Select(u => (UserModel)u);
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                userModels = userModels.Where(u =>
+                    !string.IsNullOrWhiteSpace(u.DisplayName) && u.DisplayName.Contains(search) || 
+                    !string.IsNullOrWhiteSpace(u.Mail) && u.Mail.Contains(search) || 
+                    u.OtherMails.Any(o => !string.IsNullOrWhiteSpace(o) && o.Contains(search))
+                                             );
+            }
+
+            if (page <= 0) page = 1;
+
+            //user list to prevent multiple enumeration
+            var userModelList = userModels.ToList();
+
+            var totalCount = userModelList.Count;
+            var models = userModelList.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            var userPageModel = new PagerModel<UserModel>()
+            {
+                Data = models,
+                PageSize = pageSize,
+                TotalCount = totalCount,
+            };
+
+            return userPageModel;
+        }
+    }
+}
---
+++ UserClient.cs
@@ -0,0 +1,173 @@
+﻿using GraphApi.Extensions;
+
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GraphApi.Clients
+{
+    public class UserClient
+    {
+        private readonly GraphServiceClient _graphClient;
+
+        public UserClient(string appId, string tenantId, string clientSecret)
+        {
+            // Initialize the client credential auth provider
+            var confidentialClientApplication = ConfidentialClientApplicationBuilder
+                .Create(appId)
+                .WithTenantId(tenantId)
+                .WithClientSecret(clientSecret)
+                .Build();
+            var authProvider = new ClientCredentialProvider(confidentialClientApplication);
+
+            // Set up the Microsoft Graph service client with client credentials
+            _graphClient = new GraphServiceClient(authProvider);
+        }
+
+        public async Task<IList<User>> Users()
+        {
+            // Get all users (one page)
+            var result = await _graphClient.Users
+                .Request()
+                .Select("DisplayName,Surname,GivenName,Mail,OtherMails,Id,Identities")//this will return the full user object with all OTHER properties as null.
+                                                                                      //Later select these properties to display them.
+                .GetAsync();
+
+            return result.CurrentPage;
+        }
+
+        public async Task<int> CreateUsers(IList<User> users)
+        {
+            List<BatchResponseContent> returnedResponses = new List<BatchResponseContent>();
+
+            while (users.Any())
+            {
+                //limited to 20 per request
+                var userBatch = users.GetAndRemove(20);
+                var batchRequestContent = new BatchRequestContent();
+
+                //Create a json batch request
+                foreach (var newUser in userBatch)
+                {
+                    var jsonUser = _graphClient.HttpProvider.Serializer.SerializeAsJsonContent(newUser);
+                    var addUserRequest = _graphClient.Users.Request().GetHttpRequestMessage();
+                    addUserRequest.Method = HttpMethod.Post;
+                    addUserRequest.Content = jsonUser;
+                    var addUserRequestId = batchRequestContent.AddBatchRequestStep(addUserRequest);
+                }
+                var returnedResponse = await _graphClient.Batch.Request().PostAsync(batchRequestContent);
+                returnedResponses.Add(returnedResponse);
+            }
+
+            int count = 0;
+
+            try
+            {
+                //parse the responses and check for errors
+                foreach (var returnedResponse in returnedResponses)
+                {
+                    var response = await returnedResponse.GetResponsesAsync();
+                    foreach (var responseKey in response.Keys)
+                    {
+                        var value = response[responseKey];
+                        if (!value.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine($"Create user failed. Response Key: {value.Content}");
+                        }
+                        else
+                        {
+                            count++;
+                        }
+                    }
+
+                    Console.WriteLine($" {count} users created.");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Create user failed: {ex.Message}");
+            }
+            return count;
+        }
+
+        public async Task<User> CreateUser(User user)
+        {
+            var newUser = await _graphClient.Users
+                .Request()
+                .AddAsync(user);
+
+            return newUser;
+        }
+
+        public async Task UpdateUser(User user)
+        {
+            await _graphClient.Users[user.Id]
+                    .Request()
+                    .UpdateAsync(user);
+        }
+
+        public async Task<User> GetUser(string id)
+        {
+            var user = await _graphClient.Users[id]
+                .Request()
+                .Select("DisplayName,Surname,GivenName,Mail,OtherMails,Id,Identities")//this will return the full user object with all OTHER properties as null.
+                                                                                      //Later select these properties to display them.
+                .GetAsync();
+
+            return user;
+        }
+
+        public async Task ResetPassword(string id)
+        {
+            //https://docs.microsoft.com/en-us/azure/active-directory-b2c/microsoft-graph-get-started?tabs=app-reg-ga#enable-user-delete-and-password-update
+
+            var user = new User
+            {
+                PasswordProfile = new PasswordProfile
+                {
+                    ForceChangePasswordNextSignIn = true,
+                }
+            };
+
+            await _graphClient.Users[id]
+                .Request()
+                .UpdateAsync(user);
+        }
+
+        public async Task UpdatePassword(string id, string password)
+        {
+            //https://docs.microsoft.com/en-us/azure/active-directory-b2c/microsoft-graph-get-started?tabs=app-reg-ga#enable-user-delete-and-password-update
+            
+            var user = new User
+            {
+                Id = id,
+                PasswordProfile = new PasswordProfile
+                {
+                    Password = password,
+                    ForceChangePasswordNextSignIn = false,
+                }
+            };
+
+            await _graphClient.Users[id]
+                .Request()
+                .UpdateAsync(user);
+        }
+
+        public async Task DeleteUser(string id)
+        {
+            //https://docs.microsoft.com/en-us/azure/active-directory-b2c/microsoft-graph-get-started?tabs=app-reg-ga#enable-user-delete-and-password-update
+            
+            await _graphClient.Users[id]
+                .Request()
+                .DeleteAsync();
+        }
+    }
+}
